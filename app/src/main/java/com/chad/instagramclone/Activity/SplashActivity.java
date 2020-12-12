98c54263293bcd78b557b5d57b6d1b637ad55c9a
Chad <keithchad10@outlook.com>
2020-12-12 10:40:47 +0300

    splash screen

---
+++ app/src/main/java/com/chad/instagramclone/Activity/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.chad.instagramclone.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.chad.instagramclone.Authentication.RegisterActivity;
+import com.chad.instagramclone.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        initialize();
+    }
+
+    private void initialize() {
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        }, 2000);
+    }
+}
